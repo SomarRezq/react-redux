acd6e58e963d9573731d8a5f8a4d3341128b9699
@somar.rezq <somar.rezq@gmail.com>
2022-09-18 02:34:28 +0200

    complete back end

--- Back-end/Controllers/index.js
+++ Back-end/Controllers/index.js
@@ -1,4 +1,4 @@
-const weatherReadings = [
+let weatherReadings = [
     {
         id: 0,
         temprature: 20,
@@ -19,17 +19,49 @@ const weatherReadings = [
     }
 ]
 
+
 getAllReadings = (req, res) => {
     res.status(200).json(weatherReadings);
 };
 
+
+
+//localhost:5000/weather/1
 getReading = (req, res) => {
+    let id = req.params.id;
+    for (let i = 0; i < weatherReadings.length; i++) {  
+        if (weatherReadings[i].id == id) {
+            res.status(200).json(weatherReadings[i]);
+        }
+    }    
 };
 
+
+
+// {
+//     "id": 4,
+//     "temprature": 40,
+//     "humidity": "10%",
+//     "city": "hama"
+// }
 addReading = (req, res) => {
+    let reading = req.body;
+    weatherReadings.push(reading);
+    console.log(weatherReadings)
+    res.status(200).json();
 };
 
+//localhost:5000/weather/1
 deleteReading = (req, res) => {
+    let id = req.params.id;
+    let newWeatherReading = [];
+    for (let i = 0; i < weatherReadings.length; i++) {  
+        if (weatherReadings[i].id != id) {
+            newWeatherReading.push(weatherReadings[i]);
+        }
+    }
+    weatherReadings = newWeatherReading;
+    res.status(200).json();
 };
 
-module.exports = {getAllReadings}
+module.exports = {getAllReadings, getReading, deleteReading, addReading}
--- Back-end/Routs/index.js
+++ Back-end/Routs/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
-const {getAllReadings} = require('../Controllers/index')
+const {getAllReadings, getReading, addReading, deleteReading} = require('../Controllers/index')
 router.route('/').get((req, res) => getAllReadings(req, res));
+router.route('/:id').get((req, res) => getReading(req, res));
+router.route('/').post((req, res) => addReading(req, res) );
+router.route('/:id').delete((req, res) => deleteReading(req, res) );
 
 module.exports = router;
